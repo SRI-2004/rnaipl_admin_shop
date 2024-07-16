7f790cdb69e34aaec4582c9a2b109cb2a833540d
SRI-2004 <ss1160@srmist.edu.in>
2024-07-16 16:08:21 +0530

    update cors

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ require('dotenv').config();
 const app = express();
 
 // Use the CORS middleware
-app.use(cors()); // This will allow all origins by default
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
