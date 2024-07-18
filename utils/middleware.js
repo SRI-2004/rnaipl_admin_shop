44fe778718c51f3bb2cc43f5adababa09a17d89f
SRI-2004 <ss1160@srmist.edu.in>
2024-07-18 11:46:45 +0530

    edited middle ware

--- utils/middleware.js
+++ utils/middleware.js
@@ -7,12 +7,12 @@ const verifyToken = (req, res, next) => {
   const tokenBearer = req.headers['authorization'];
   const token = tokenBearer.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(402).json({ message: 'No token provided' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return res.status(402).json({ message: 'Invalid token' });
     }
     req.user = decoded; 
     next(); 
